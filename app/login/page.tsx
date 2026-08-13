a10e6991fd85bfde0e1b3711996dd097a276eb84
genday256@gmail.com <genday256@gmail.com>
2026-08-13 08:38:45 -0400

    Correccion de tipeo en useRouter del login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,7 +11,7 @@ export default function LoginPage() {
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState<{ type: 'error' | 'success'; text: string } | null>(null);
   const [loading, setLoading] = useState(false);
-  const router = Router = useRouter();
+  const router = useRouter();
 
   const handleAuth = async (e: React.FormEvent) => {
     e.preventDefault();
